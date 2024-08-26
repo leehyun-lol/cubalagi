b6785a18a302e2f4fc5f9ab051b36d6088f0ea3c
leehyun-lol <leehyunsang3112@gmail.com>
2024-08-26 16:06:19 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args) {
+        debug();
+    }
+    
+    public static void debug() {
+        int x = 50;
+        if (x < 5) {
+            System.out.println("lesser");
+        } else {
+            System.out.println("greater");
+        }
+    }
+}
